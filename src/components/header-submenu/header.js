f9c4137ba92837bb8642627e7204ebc0494346e0
Kustiche <ChernichnyKustik@mail.ru>
2022-12-04 09:09:58 +0300

    Разделил шапку на без подменю и с подменю

--- src/components/header-submenu/header.js
+++ src/components/header-submenu/header.js
@@ -26,6 +26,21 @@ const isMobile = {
   }
 };
 
+if (isMobile.any()) {
+  document.body.classList.add('touch');
+  let menuArrows = document.querySelectorAll('.menu__arrow');
+  if (menuArrows.length > 0){
+    for (let index = 0; index < menuArrows.length; index++) {
+      const menuArrow = menuArrows[index];
+      menuArrow.addEventListener('click', function(e) {
+        menuArrow.parentElement.classList.toggle('active')
+      });
+    }
+  }
+} else {
+  document.body.classList.add('pc');
+}
+
 //меню бургер
 
 const menuBurger = document.querySelector('.burger');
